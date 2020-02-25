6fad1958dd0e6240b385329931c736c7a68e6de1
Bor-is-luv <zverkii5@gmail.com>
2020-02-26 01:05:05 +0300

    end teacher form

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import cabinet_view, CreateGroupView, CreateCourseView
+from .views import cabinet_view, CreateGroupView, CreateCourseView, CreateTeacherView
 
 
 urlpatterns = [
     path('', cabinet_view, name='cabinet_page'),
     path('create_group/', CreateGroupView.as_view(), name='create_group_page'),
     path('create_course/', CreateCourseView.as_view(), name='create_course_page'),
-
+    path('create_teacher/', CreateTeacherView.as_view(), name='create_teacher_page'),
 ]
--- registration/urls.py
+++ registration/urls.py
@@ -4,6 +4,6 @@ from .views import UserLoginView, UserRegisterView, UserLogoutView  # *
 
 urlpatterns = [
     path('', UserLoginView.as_view(), name='login_page'),
-    path('register', UserRegisterView.as_view(), name='register_page'),
+    path('register', UserRegisterView.as_view(), name='registration_page'),
     path('logout', UserLogoutView.as_view(), name='logout_page'),
 ]
