e03ccc545d39783ad241b73b48b052bbdd2170e5
nekio <nikita.volchenkov1@gmail.com>
2020-06-16 02:20:36 +0300

    Registration app's templates, static files and css refactoring

--- djumbi4/settings.py
+++ djumbi4/settings.py
@@ -116,6 +116,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
 LOGIN_URL = 'login_page'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
