8403ef0b572b420705dab7376a8d8d241c9a94f1
nekio <nikita.volchenkov1@gmail.com>
2020-07-05 23:51:56 +0300

    new static directories

--- djumbi4/settings.py
+++ djumbi4/settings.py
@@ -115,12 +115,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "scripts"),
+    os.path.join(BASE_DIR, 'scripts/'),
+    os.path.join(BASE_DIR, 'styles/'),
 ]
 
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_URL = 'login_page'
 
@@ -134,5 +137,3 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-МEDIA_URL = '/media/' 
